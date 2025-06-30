071ded845e5c4c72f33f8698a880f8298aa8b5d4
Artur Guilherme <132027060+ArturMaidana@users.noreply.github.com>
2025-06-29 22:52:22 -0400

    Add files via upload

---
+++ atividade01.py
@@ -0,0 +1,42 @@
+# Saudação
+print("Hello, World");
+
+#Calculadora de soma
+numero1 = 14
+numero2 = 24
+soma = numero1 + numero2
+
+print("A soma dos valores é: ", soma);
+
+#calculadora de Volume
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print(f"O volume da caixa é de {volume} cm³");
+
+# Calculadora de Preço Total
+NomeProduto = "Teclado Gamer"
+PrecoUnitario = 12.40
+quantidade = 3
+
+PrecoTotal = PrecoUnitario * quantidade
+
+print(f"Produto: {NomeProduto}")
+print(f"Preço Unitário: R$ { PrecoUnitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço Total: R$ { PrecoTotal:.2f}")
+
+#Calculadora de Número inteiro
+A = int(input("Digite o primeiro valor (A): "))
+B = int(input("Digite o primeiro valor (B): "))
+C = int(input("Digite o primeiro valor (C): "))
+D = int(input("Digite o primeiro valor (D): "))
+
+DIFERENCA = (A * B - C * D)
+
+print(f"DIFERENCA: {DIFERENCA}")
+
